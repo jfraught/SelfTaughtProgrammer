4d9569e7b968e77b6a954be13c9544719ca37fc3
Jordan Fraughton <i25203@dxvln1q1f8jc.xactware.com>
2019-08-08 15:42:19 -0600

    Chapter 3

---
+++ chapter_3.py
@@ -0,0 +1,36 @@
+#1
+print("This")
+print("Is")
+print("Strings")
+
+#2
+if x < 10:
+    print("X is less than 10")
+else:
+    print("X is greater or equal to 10")
+
+#3
+if x <= 10:
+    print("X is less than or equal to 10")
+elif x > 10 and x <= 25:
+    print("X is greater than 10 and less than or equal to 25")
+else:
+    print("X is greater than 25")
+
+#4
+x = 10 % 2
+print(x)
+
+#5
+x = 10
+y = 2
+z = x // y 
+print(z)
+
+#6
+age = 10 
+if age <= 10:
+    print("You're not old enough to ride this ride")
+else:
+    print("Enjoy the ride!")
+
